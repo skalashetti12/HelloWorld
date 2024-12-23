1b4230da71ce9f651a2183fb19ce6c2bf5a7a62d
skalashetti12 <shreeshailkalashetti40@gmail.com>
2024-12-23 13:29:27 +0000

    Added the few lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -20,6 +20,8 @@ public class HelloWorld {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
 
